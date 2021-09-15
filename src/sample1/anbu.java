6a7f39c635c90f5ec316591a8a8400c1a02b35b2
Anbarasan <anbuv2000@gmail.com>
2021-09-15 13:18:04 +0530

    update success

---
+++ src/sample1/anbu.java
@@ -0,0 +1,11 @@
+package sample1;
+
+public class anbu {
+	public static void main(String[] args) {
+		System.out.println("142");
+		System.out.println("14");
+		System.out.println("12");
+		System.out.println("42");
+	}
+
+}
